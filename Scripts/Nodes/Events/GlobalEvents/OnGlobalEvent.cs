e87b3e4f56a7efc8d74170797aa60c10ae646c47
Luis Rojas <luis.andres81@gmail.com>
2019-01-15 00:27:22 +0100

    New flow event, OnGlobalEvent

---
+++ Scripts/Nodes/Events/GlobalEvents/OnGlobalEvent.cs
@@ -0,0 +1,17 @@
+﻿using HalfBlind.ScriptableVariables;
+using XNode;
+
+namespace Events.Global {
+    public class OnGlobalEvent : EventNode {
+        public ScriptableGameEvent GameEvent;
+
+        protected override void Init() {
+            base.Init();
+            GameEvent?.AddListener(TriggerFlow);
+        }
+
+        public override object GetValue(NodePort port) {
+            return null;
+        }
+    }
+}
