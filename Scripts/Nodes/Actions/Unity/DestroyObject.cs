7c277729dae389e826fde8e98b34f19558ed5a19
Luis Rojas <luis.andres81@gmail.com>
2019-01-16 00:16:08 +0100

    Flow node to destroy objects

---
+++ Scripts/Nodes/Actions/Unity/DestroyObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using XNode;
+
+namespace Actions.UnityNative {
+    public class DestroyObject : FlowNode {
+        [Input] public GameObject Target;
+
+        public override void ExecuteNode() {
+            var toDestroy = GetInputValue<GameObject>(nameof(Target), Target);
+            Destroy(toDestroy);
+        }
+
+        public override object GetValue(NodePort port) {
+            return null;
+        }
+    }
+}
