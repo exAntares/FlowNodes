525b37417d090197552eeb7091aecdaf048cc29c
Luis Rojas <luis.andres81@gmail.com>
2019-01-25 00:18:16 +0100

    New OnStart node

---
+++ Scripts/Nodes/Events/Unity/OnStart.cs
@@ -0,0 +1,14 @@
+﻿using XNode;
+
+namespace Events.UnityNative {
+    [CreateNodeMenu("Events/OnStart", "Awake", "Start", "Init")]
+    public class OnStart : EventNode {
+        private void Start() {
+            TriggerFlow();
+        }
+
+        public override object GetValue(NodePort port) {
+            return null;
+        }
+    }
+}
