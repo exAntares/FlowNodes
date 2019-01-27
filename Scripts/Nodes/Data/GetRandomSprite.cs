5efeb665fb9b794170e5a7e1749c4cd1502f0ecd
Luis Rojas <luis.andres81@gmail.com>
2019-01-27 23:11:52 +0100

    New get random sprite

---
+++ Scripts/Nodes/Data/GetRandomSprite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using XNode;
+
+namespace HalfBlind.Nodes {
+    [CreateNodeMenu("Random/" + nameof(GetRandomSprite), "Random", "Sprite")]
+    public class GetRandomSprite : MonoNode {
+        [Input] public Sprite[] Sprites;
+        [Output] public Sprite Output;
+
+        public override object GetValue(NodePort port) {
+            if(port.fieldName == nameof(Output)) {
+                var sprites = GetInputValue(nameof(Sprites), Sprites);
+                if (sprites != null && sprites.Length > 0) {
+                    var index = Random.Range(0, sprites.Length);
+                    return sprites[index];
+                }
+            }
+            return null;
+        }
+    }
+}
