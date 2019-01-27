7ec5ae5a0c79a8d3c16eeccee0f76740b7e48222
Luis Rojas <luis.andres81@gmail.com>
2019-01-27 23:11:39 +0100

    New PlayAnimation node

---
+++ Scripts/Nodes/Actions/Unity/PlayAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using XNode;
+
+namespace HalfBlind.Nodes {
+    [CreateNodeMenu("Animation/"+nameof(PlayAnimation), "Play", "Animation", "Animator")]
+    public class PlayAnimation : FlowNode {
+        [Input] public Animator Target;
+        [Input] public string StateName;
+
+        public override void ExecuteNode() {
+            var animator = GetInputValue(nameof(Target), Target);
+            var stateName = GetInputValue(nameof(StateName), StateName);
+            animator.Play(stateName);
+        }
+
+        public override object GetValue(NodePort port) {
+            return null;
+        }
+    }
+}
